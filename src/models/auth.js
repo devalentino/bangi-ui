18db96458a14703026b4c00d40a3dde87b4c6499
devalentino <valentin.saulyak@gmail.com>
2025-08-11 22:00:54 +0300

    Statistics page

--- src/models/auth.js
+++ src/models/auth.js
@@ -1,39 +1,42 @@
-m = require("mithril")
+m = require("mithril");
 
 var Authentication = {
-    username: null,
-    password: null,
-    isAuthenticated: false,
+  username: null,
+  password: null,
+  isAuthenticated: false,
+  token: null,
 
-    signIn: function(username, password) {
-        Authentication.username = username
-        Authentication.password = password
+  signIn: function (username, password) {
+    Authentication.username = username;
+    Authentication.password = password;
 
-        let token = btoa(`${Authentication.username}:${Authentication.password}`)
+    Authentication.token = btoa(
+      `${Authentication.username}:${Authentication.password}`,
+    );
 
-        m.request({
-            method: "POST",
-            url: "http://i-cosanzeana.me/api/v2/auth/authenticate",
-            headers: {'Authorization': `Basic ${token}`}
-        })
-        .then(function(result) {
-            Authentication.isAuthenticated = true
-            m.route.set('')
-        })
-        .catch(function(){
-            alert('Failed to authenticate')
-            Authentication.isAuthenticated = false
-            m.route.set('')
-        })
-    },
+    m.request({
+      method: "POST",
+      url: "http://i-cosanzeana.me/api/v2/auth/authenticate",
+      headers: { Authorization: `Basic ${Authentication.token}` },
+    })
+      .then(function (result) {
+        Authentication.isAuthenticated = true;
+        m.route.set("");
+      })
+      .catch(function () {
+        alert("Failed to authenticate");
+        Authentication.isAuthenticated = false;
+        m.route.set("");
+      });
+  },
 
-    signOut: function() {
-        Authentication.username = null
-        Authentication.password = null
-        Authentication.isAuthenticated = false
-        m.route.set('')
-        alert('Signed out')
-    }
-}
+  signOut: function () {
+    Authentication.username = null;
+    Authentication.password = null;
+    Authentication.isAuthenticated = false;
+    m.route.set("");
+    alert("Signed out");
+  },
+};
 
-module.exports = {Authentication}
+module.exports = { Authentication };
