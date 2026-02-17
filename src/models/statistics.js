b1c87d7c6d32d07da24eb920190900ab40b5e1be
Valentyn Sauliak <valentin.saulyak@gmail.com>
2026-02-17 14:20:06 +0200

    minor code fixes

--- src/models/statistics.js
+++ src/models/statistics.js
@@ -82,9 +82,9 @@ class StatisticsModel {
       url: `${process.env.BACKEND_API_BASE_URL}/reports/statistics`,
       params: parameters,
     }).then(function (payload) {
-      this.report = payload.content.report || {};
-      this.parameters = payload.content.parameters || [];
-      this.groupParameters = payload.content.groupParameters || [];
+      this.report = payload.content.report;
+      this.parameters = payload.content.parameters;
+      this.groupParameters = payload.content.groupParameters;
       if (typeof this.groupParameters === "string") {
         this.groupParameters = this.groupParameters
           .split(",")
