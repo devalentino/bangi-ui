d96b6b715e7da421bd3d24b45397b2e932590f3a
Valentin Saulyak <valentin.saulyak@gmail.com>
2026-02-19 17:15:37 +0200

    Fix docker image build process (#13)

--- docker/40-generate-app-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-cat > /usr/share/nginx/html/app-config.js <<EOF
-window.APP_CONFIG = window.APP_CONFIG || {
-  BACKEND_API_BASE_URL: "${BACKEND_API_BASE_URL:-http://localhost:8080}"
-};
-EOF
